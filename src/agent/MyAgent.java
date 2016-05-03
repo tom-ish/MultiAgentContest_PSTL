9398403b24cbb5f90fd47fd80519d6449d5e8dd8
tom-ish <tomohiro.ishiwata@gmail.com>
2016-05-03 18:43:46 +0200

    agent package added

--- src/agent/MyAgent.java
+++ src/agent/MyAgent.java
@@ -1,5 +1,26 @@
 package agent;
 
-public class MyAgent {
+import eis.iilang.Action;
+import eis.iilang.Percept;
+import massim.javaagents.Agent;
+
+public class MyAgent extends Agent {
+
+	public MyAgent(String name, String team) {
+		super(name, team);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handlePercept(Percept arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Action step() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

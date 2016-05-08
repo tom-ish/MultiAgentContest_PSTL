833deeaa7bfa896a521d0a2497d299818628f92c
datng <nguyendatci@hotmail.com>
2016-05-09 00:10:28 +0200

    testing

--- src/massim/javaagents/agents/MyAgent.java
+++ src/massim/javaagents/agents/MyAgent.java
@@ -1,5 +1,6 @@
 package massim.javaagents.agents;
 
+import java.util.Iterator;
 import java.util.List;
 
 import eis.iilang.Action;
@@ -25,13 +26,33 @@ public class MyAgent extends Agent {
 
 	@Override
 	public Action step() {
+		
 		for(Percept p : getAllPercepts()){
-			
-			List<Parameter> list = p.getParameters();
-			for(Parameter param : list){
-				System.out.println(param.toString());
-				System.out.println("-----------------------------------");
+			if(p.getName().equalsIgnoreCase("auctionJob")){
+				
 			}
+			if(p.getName().equalsIgnoreCase("pricedJob")){
+//				System.out.println(p.toXML());
+				List<Parameter> list = p.getParameters();
+				Parameter id = list.get(0);
+				Parameter storage = list.get(1);
+				Parameter begin = list.get(2);
+				Parameter end = list.get(3);
+				Parameter reward = list.get(4);
+				Parameter itemName = list.get(5);
+//				Parameter amount = list.get(6);
+//				if(list.get(7) != null){
+//					Parameter delivered = list.get(7);
+//				}
+				System.out.println("Storage : "+ storage.toString());
+				return CityUtil.action("goto", "facility="+storage.toString());
+				
+			}
+			
+//			for(Parameter param : list){
+//				System.out.println(param.toString());
+//				System.out.println("-----------------------------------");
+//			}
 		}
         
 		return CityUtil.action("goto", "facility=shop1");

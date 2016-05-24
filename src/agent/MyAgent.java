ee2dec9786035c2398a9662911d0e2f5342b913c
tom-ish <tomohiro.ishiwata@gmail.com>
2016-05-24 20:01:45 +0200

    my

--- src/agent/MyAgent.java
+++ src/agent/MyAgent.java
@@ -1,170 +1,86 @@
 package agent;
 
+import apltk.interpreter.data.LogicGoal;
 import massim.javaagents.Agent;
 import massim.javaagents.agents.CityUtil;
-import apltk.interpreter.data.LogicBelief;
 import eis.iilang.Action;
-import eis.iilang.Parameter;
 import eis.iilang.Percept;
+import eis.iilang.Parameter;
+
 
 public class MyAgent extends Agent {
 	
-	private Role role;
 
-	public MyAgent(String name, String team, String role) {
+	public MyAgent(String name, String team) {
 		super(name, team);
-		// TODO do something if necessary
-		this.role = Role.valueOf(Role.class, role);
+		// TODO Auto-generated constructor stub
 	}
-	
-	public void setRole(Role role){ this.role = role; }
-	public Role getRole(){ return this.role; }
 
-	@Override
-	public void handlePercept(Percept arg0) {
-		// TODO handle percepts if necessary
-
-	}
+	private boolean startRole = false;
+	private Role role = Role.JobSelector;
 
 	@Override
 	public Action step() {
-		// TODO deliberate and Parameter action	
-		if(this.role == Role.JobSelector){
-			for(Percept percept : getAllPercepts()){
-				// On ne considère uniquement les pricedJob pour la simulation minimale
-				if(percept.getName().equalsIgnoreCase("pricedJob")){
-					Parameter itemsForJob = percept.getParameters().get(5);
-					System.out.println("========================================");
-					System.out.println(itemsForJob.toString());
+		if(role == Role.JobSelector){
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			selectJob();
+			sortNeededItems();
+			
+			return new Action("skip");		
+		}
+		return null;
+	}
+	
+	private void selectJob() {
+		int seuil = 0;
+		this.clearGoals();
+		for(Percept p : getAllPercepts()) {
+			if(p.getName().equalsIgnoreCase("auctionJob")){
+				for(Parameter param : p.getParameters()){
+					System.out.println("======================> "+param.toString());
+					// parser les informations contenues dans les jobs
+					// <auctionJob id="job_id1" storage="stor1" begin="35" end="85" fine="100" maxBid="200">
+					// <pricedJob id="job_id2" storage="stor2" begin="50" end="120" reward="150">
+					String[] infos = param.toString().split(" ");
+					String jobType = p.getName();
+					String begin = infos[2];
+					String end = infos[3];
+					Integer duration = Integer.valueOf(end) - Integer.valueOf(begin);
+					// Si la durée du job est supérieure au seuil défini, c'est que le job est réalisable donc on l'ajoute aux Goals
+					if(duration > seuil) {
+						// new LogicGoal(String predicate, Collection<String> parameters)
+						this.addGoal(new LogicGoal(jobType, param.toString()));
+					}
+					// sinon on skip le job car on considère qu'il ne pourra pas etre finalisé
+					else {
+						System.err.println("Job non ajouté");
+					}
+				}
+			}
+			else if(p.getName().equalsIgnoreCase("pricedJob")){
+				for(Parameter param : p.getParameters()){
+					String jobType = p.getName();
 				}
 			}
 		}
-		else if(this.role == Role.Acheteur){
+	}
+	
+	private void sortNeededItems() {
+		for(Percept p : getAllPercepts()) {
 			
 		}
-		else if(this.role == Role.JobDeliver){
-			
-		}
-		else if(this.role == Role.Rechargeur){
-			
-		}
-
-		// 1). Parcourir tous les Percepts
-		for(Percept percept : getAllPercepts()){
-
-			// 1.1). Mettre à jour les Beliefs
-			switch(percept.getName()) {
-			case "charge":
-				updateChargeBelief(percept);
-				break;
-				/*
-			case "entity":
-				updateEntityBelief(percept);
-				break;*/
-			case "inFacility":
-				updateInFacilityBelief(percept);
-				break;
-			case "item":
-				updateItemBelief(percept);
-				break;
-			case "jobTaken":
-				updateJobTakenBelief(percept);
-				break;
-			case "lat":
-				updateLatBelief(percept);
-				break;
-			case "load":
-				updateLoadBelief(percept);
-				break;
-			case "lon":
-				updateLonBelief(percept);
-				break;
-			case "lastAction":
-				updateLastActionBelief(percept);
-				break;
-			case "lastActionParam":
-				updateLastActionParamBelief(percept);
-				break;
-			case "lastActionResult":
-				updateLastActionResultBelief(percept);
-				break;
-			case "pricedJob":
-				updatePricedJobBelief(percept);
-				break;
-			default:;
-			}
-
-
-
-			// 1.2). Mettre à jour les Goals
-		}
-
-
-		// 2). Générer l'action à effectuer
-
-		return CityUtil.action("goto", "facility=shop1");
 	}
 
-	private void updateChargeBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // current energy charge
+	@Override
+	public void handlePercept(Percept p) {
+		// TODO Auto-generated method stub
+		
 	}
-/*
-	private void updateEntityBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-	}
-	*/
-	private void updateInFacilityBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // facility or none
-	}
+
 	
-	private void updateItemBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().get(0).toString()); // item name
-		belief.getParameters().add(p.getParameters().get(1).toString()); // amount
-	}
-	
-	private void updateJobTakenBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // job id
-	}
-	
-	private void updateLatBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // latitude
-	}
-	
-	private void updateLoadBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // current load of the agent
-	}
-	
-	private void updateLonBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // longitude
-
-	}
-	
-	private void updateLastActionBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // last action id
-	}
-	
-	private void updateLastActionParamBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // last action parameters
-	}
-	
-	private void updateLastActionResultBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // result of the last action
-	}
-	
-	private void updatePricedJobBelief(Percept p){
-		LogicBelief belief = new LogicBelief(p.getName());
-		belief.getParameters().add(p.getParameters().getFirst().toString()); // lat
-	}
-
-
 }
